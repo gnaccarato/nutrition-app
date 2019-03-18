7fb35e649f01d6bb17348b8d8800f3231cede718
Giovanni Naccarato <naccarato2005@gmail.com>
2019-03-18 12:21:28 -0400

    Updates

---
+++ precache-manifest.13f1e1b071710c5a7c645400828f5b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0870f0f2be637df39ed",
+    "url": "/nutrition-app/static/css/main.86b655e7.chunk.css"
+  },
+  {
+    "revision": "a0870f0f2be637df39ed",
+    "url": "/nutrition-app/static/js/main.ab1719b3.chunk.js"
+  },
+  {
+    "revision": "9ea9a0025739f3f8c838",
+    "url": "/nutrition-app/static/js/runtime~main.aff253eb.js"
+  },
+  {
+    "revision": "ac4df100552dd8026d07",
+    "url": "/nutrition-app/static/css/2.73d5622e.chunk.css"
+  },
+  {
+    "revision": "ac4df100552dd8026d07",
+    "url": "/nutrition-app/static/js/2.b3e6ddd7.chunk.js"
+  },
+  {
+    "revision": "35d2d16bd38288b1aefc8f4382977d3f",
+    "url": "/nutrition-app/index.html"
+  }
+];
